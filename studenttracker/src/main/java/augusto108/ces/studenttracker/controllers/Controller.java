ff057b8db99a8716ffbbb05c1f032f6209c31eb1
César Augusto <ces.augusto108@gmail.com>
2023-04-17 09:18:07 -0300

    Controller class links fixed

--- studenttracker/src/main/java/augusto108/ces/studenttracker/controllers/Controller.java
+++ studenttracker/src/main/java/augusto108/ces/studenttracker/controllers/Controller.java
@@ -6,28 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = {"/", ""})
 public class Controller {
     @GetMapping
     public ResponseEntity<List<Link>> getAppAggregateRoots() throws NoSuchMethodException {
-        Method getAddresses = AddressController.class.getMethod("getAddresses", Map.class);
-        Method getCampuses = CampusController.class.getMethod("getCampuses", Map.class);
-        Method getStudents = StudentController.class.getMethod("getStudents", Map.class);
-        Method getUndergraduatePrograms = UndergraduateProgramController.class
-                .getMethod("getUndergraduatePrograms", Map.class);
-
-        Link addressesLink = linkTo(getAddresses).withRel("addresses");
-        Link campusesLink = linkTo(getCampuses).withRel("campuses");
-        Link studentsLink = linkTo(getStudents).withRel("students");
-        Link undergraduateProgramsLink = linkTo(getUndergraduatePrograms).withRel("undergraduatePrograms");
+        Link addressesLink = linkTo(methodOn(AddressController.class).getAddresses(0, 5, new HashMap<>())).withRel("addresses");
+        Link campusesLink = linkTo(methodOn(CampusController.class).getCampuses(0, 5, new HashMap<>())).withRel("campuses");
+        Link studentsLink = linkTo(methodOn(StudentController.class).getStudents(0, 5, new HashMap<>())).withRel("students");
+        Link undergraduateProgramsLink = linkTo(methodOn(UndergraduateProgramController.class).getUndergraduatePrograms(0, 5, new HashMap<>()))
+                .withRel("undergraduatePrograms");
 
         return ResponseEntity.ok(Arrays.asList(addressesLink, campusesLink, studentsLink, undergraduateProgramsLink));
     }
